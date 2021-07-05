72671f1032588262be62b3eae735682261f68566
Nohely Badillo <nohely.badillo96@gmail.com>
2021-07-05 13:26:27 -0500

    cambios en codigo de cipher luego de hacer pseudo codigo

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,22 @@
-const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+let desplazamiento = 4;
+let mensaje = "MI MENSAJE SECRETO";
+
+mensaje = mensaje.trim();
 
 
+//se define la función para nuestro cipher
+function encode(string, offset) {
+    let valorAscii = mensaje.charCodeAt(i);
 
-const cipher = {
-    // ...
-};
+    //como las mayusculas se encuentran en un rango de 65 y 90 en valores ascii entonces para nuestra formula de cifrado
+    if (valorAscii >= 65 && <= 90) {
+        desplazo = (valorAscii - 65 + 3) % 26 + 65;
+    }
+}
 
-export default cipher;
+
+export function encode(cipher) {
+    console.log
+}
