50a3cb0a361ff9198d934b5ef551fea3af931846
Nohely Badillo <nohely.badillo96@gmail.com>
2021-07-19 10:44:31 -0500

    update gh page

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 import cipher from './cipher.js';
 
 
-const output = document.getElementById("output");
+const output = document.getElementById("output"); //se decalara output como global ya que se usará continuamente
 
 
 document.getElementById("cifrar").addEventListener("click", (event) => {
@@ -16,22 +16,29 @@ document.getElementById("cifrar").addEventListener("click", (event) => {
         alert("¡No olvides introducir tu mensaje!")
     }
     let result = cipher.encode(offset, string);
-    output.innerHTML = result;
+    output.innerHTML = result; //imprime el resultado de la función cipher en este caso encode
 });
 
 
 document.getElementById("descifrar").addEventListener("click", (event) => {
     event.preventDefault();
+    //se repite la estructura de input y output hecha en encode
     let offset = Number(document.getElementById('offset').value);
+    if (offset == "") {
+        alert("¡No olvides introducir el valor de tu desplazamiento!")
+    }
     let string = document.getElementById('string').value;
+    if (string == "") {
+        alert("¡No olvides introducir tu mensaje!")
+    }
     let result = cipher.decode(offset, string);
-    output.innerHTML = result;
+    output.innerHTML = result; //resultado decode
 });
 
 
 document.getElementById("copiar").addEventListener("click", (event) => {
     event.preventDefault();
-    output.select();
-    output.setSelectionRange(0, 999999);
-    document.execCommand("copy");
+    output.select(); //selecciona el texto ubicado en el output
+    output.setSelectionRange(0, 999999); //le da el rango al que puede seleccionar texto
+    document.execCommand("copy"); //genera la interacción con el portapapeles
 });
